06d4947082cc5aa111a01ea28c2e74fa68962b25
Ca-Dn <Daniel.Carruzzo@formation.orif.ch>
2025-05-14 15:31:24 +0200

    Update game5.outpututilisateur.py

--- games/game5/game5.outpututilisateur.py
+++ games/game5/game5.outpututilisateur.py
@@ -1,4 +1,32 @@
 def userOutput() :
-    nextatempt = 1
-    while nextatempt == 1 :
-        num = int(input("VEuillez enfin sélectionner le nombre que vous voulez entrer (1-9)"))
+    from initGrid import is_valid
+    startloop = 1
+    modification = input("Permettre les modifications en cas d'erreur ? (Oui/Non)")
+    while startloop == 1 :
+        if modification == "Oui" :
+            startloop = 0
+        elif modification == "Non" :
+            startloop = 0
+        else :
+            modification = input("Veuillez sélectionner (Oui) ou (Non) pour la modification d'erreur : ")
+
+    nextatempt = 10
+    if modification == "Oui" :
+        while nextatempt == 10 :
+            row = int(input("Veuillez sélectionner la ligne de la case à remplir"))
+            col = int(input("Veuillez maintenant sélectionner la colonne de la case à remplir"))
+            num = int(input("Veuillez enfin sélectionner le nombre que vous voulez entrer (1-9)"))
+            if is_valid != True :
+                nextatempt = 0
+    else :
+        while nextatempt >= 1 :
+            row = int(input("Veuillez sélectionner la ligne de la case à remplir"))
+            col = int(input("Veuillez maintenant sélectionner la colonne de la case à remplir"))
+            num = int(input("Veuillez enfin sélectionner le nombre que vous voulez entrer (1-9)"))
+            if is_valid != True :
+                nextatempt = nextatempt - 1
+                print("Vous devez modifier votre dernière réponse afin de continuer.")
+            else : 
+                    grid[row][col] = num
+
+    
